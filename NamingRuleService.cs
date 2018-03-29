b3aa4e1e4ab87a5450cf2576a857b32f80c956d3
deandark92386 <37525654+deandark92386@users.noreply.github.com>
2018-03-29 11:17:18 +0800

    Add files via upload

---
+++ NamingRuleService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taoyuan.Port.VotingSystem.Service.BaseDatas
+{
+    public class NamingRuleService
+    {
+        public int TestFunction(int index)
+        {
+            switch (index)
+            {
+                case 1: break;
+                default: break;
+            }
+
+            if (index == 0)
+                DoTrue();
+            else
+                DoFalse();
+
+            //bad 上面已有動作
+            //if (DoTrue() == true) { DoTrue(); }
+
+
+            //bad 1*(1+1)/2 =1 , index = 1 
+            //return (index * (index + 1) / 2);
+
+            //god
+            return (index);
+        }
+
+        private bool DoTrue()
+        {
+            return true;
+        }
+        private bool DoFalse()
+        {
+            return false;
+        }
+
+        public void TestStringFunction(string value)
+        {
+            string NewString = "ABCDEFG"; //新字串
+            //bad 
+            if (value != null | value != "") value = NewString.Replace("A", ",").Replace("B", "_").Replace("C", " ");
+
+            //god
+            if (value != null || value != "")
+                value = NewString.Replace("A", ",").Replace("B", "_").Replace("C", " ");
+
+        }
+
+    }
+}
